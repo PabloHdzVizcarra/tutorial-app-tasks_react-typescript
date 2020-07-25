664921af286eba24874f1df204c7f8430f45a433
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-07-24 19:04:41 -0500

    finish practice projetc

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState, useRef } from "react";
 
-function App() {
+// especificamos el tipo de elemento en typescript
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormElement) => {
+    event.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  };
+
+  const addTask = (name: string): void => {
+    const newTask: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTask);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewTask(event.target.value);
+  };
+
+  const toggleDoneTask = (index: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[index].done = !newTasks[index].done;
+    setTasks(newTasks);
+  }
+
+  const removeTask = (index: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="form-control"
+                  onChange={handleChange}
+                  type="text"
+                  value={newTask}
+                  autoFocus
+                  ref={taskInput}
+                />
+                <button
+                  className="btn btn-success btn-block mt-2"
+                >Save</button>
+              </form>
+            </div>
+          </div>
+
+          {tasks.map((task: ITask, index) => (
+            <div
+              className="card card-body mt-2"
+              key={index}
+            >
+              <h2 style={{ textDecoration: task.done ? 'line-through' : '' }}>{task.name}</h2>
+              <div>
+                <button
+                  className="btn btn-secondary mr-2"
+                  onClick={() => toggleDoneTask(index)}
+                >
+                  {task.done ? '✓' : 'x'}
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeTask(index)}
+                >
+                  🗑
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootswatch/dist/lumen/bootstrap.min.css'; // importacion de boostwatch
 
 ReactDOM.render(
   <React.StrictMode>
